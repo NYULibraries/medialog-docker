f241803744e39672e7426177d0851d6ab2253a4c
Don Mennerich <don.mennerich@nyu.edu>
2019-09-03 21:43:10 -0400

    adding devmedialog repo

--- medialog/entrypoint.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-rvm install 2.0.0
-rvm gemset create medialog
-rvm alias create default 2.0.0@medialog
-rvm use default
-gem install bundler -v 1.17.3
-cd /opt/medialog/ && bundle install
