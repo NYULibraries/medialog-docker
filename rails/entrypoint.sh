5d3e6e3988b0ed61c5a09ab1999ad7bcb3e5193f
Don Mennerich <don.mennerich@nyu.edu>
2019-09-03 21:41:40 -0400

    adding devmedialog repo

---
+++ rails/entrypoint.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+cd /opt/medialog/
+gpg --keyserver hkp://pool.sks-keyservers.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://get.rvm.io | bash
+source /home/medialog/.rvm/scripts/rvm
+rvm install 2.0.0
+rvm gemset create medialog
+rvm alias create default 2.0.0@medialog
+rvm use default
+gem install bundler -v 1.17.3
+bundle install
+rails s
